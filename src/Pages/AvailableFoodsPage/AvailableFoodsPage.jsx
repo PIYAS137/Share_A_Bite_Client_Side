17d1e6e6bead55d8ab7986f3d1ddedce189bc212
S M Piyas Mahamude Alif <piyasmahmudealif@gmail.com>
2023-11-05 01:48:05 +0600

    ui for available food page is done

--- src/Pages/AvailableFoodsPage/AvailableFoodsPage.jsx
+++ src/Pages/AvailableFoodsPage/AvailableFoodsPage.jsx
@@ -1,7 +1,18 @@
+import { foodarr } from "../../Components/FeaturedFood/FeaturedFood"
+import SingleCardFeaturedFood from "../../Components/FeaturedFood/SingleCardFeaturedFood"
+
 
 const AvailableFoodsPage = () => {
   return (
-    <div>AvailableFoodsPage</div>
+    <div>
+      <h1 className="text-5xl font-bold text-center my-3">Available Food </h1>
+      <p className=" text-center pt-2 mb-10">Share your extra bites to everyone !</p>
+      <div className=" grid grid-cols-3 gap-10">
+            {
+                foodarr.map(one=><SingleCardFeaturedFood data={one} key={one.id}/>)
+            }
+        </div>
+    </div>
   )
 }
 
